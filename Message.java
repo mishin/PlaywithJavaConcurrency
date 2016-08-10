c298c79d023afc7f08a28b5c4a58b97b0d60f256
Nikolay Mishin <mi@ya.ru>
2016-08-10 13:40:24 +0300

    Create Message.java

---
+++ Message.java
@@ -0,0 +1,17 @@
+package ru.bank.service;
+
+/**
+ * Created by Mishin737 on 10.08.2016.
+ */
+public class Message {
+    private String msg;
+
+    public Message(String str){
+        this.msg=str;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+}
